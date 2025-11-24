8e364efd965ec10118177edd236ff12095f5a06f
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-11-24 14:11:36 +0000

    começado

---
+++ menu.py
@@ -0,0 +1,24 @@
+import random
+print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
+print("🟩         menu          I")
+print("🟩                       I")
+print("🟩         CAUAN         I")
+print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
+
+
+print("Qual os níveis de dificuldade?")
+print("")
+print("(1)-Fácil (2)- Médio (3)- Difícil ")
+print("")
+nível = int(input("Escolha um nível"))
+
+
+if (nível == 1): 
+print("Digite o ano de nascimento: ")
+
+print=int(input("Digite o ano atual"))
+
+idade = ano_atual - ano_nascimento
+
+print(f"Você tem {idade} anos.")
+
