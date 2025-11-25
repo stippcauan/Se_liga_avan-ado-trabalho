6f13c96c2f88a1bcd5e7d95e46e33f12e4e56bf4
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-11-25 14:12:22 +0000

    .

--- menu.py
+++ menu.py
@@ -1,53 +1,48 @@
+import datetime
+
 def calcular_idade():
     ano_nascimento = int(input("Digite seu ano de nascimento: "))
-    ano_atual= int(input("Digite que dia é hoje"))
+    ano_atual = int(input("Digite que ano nós estamos"))
     idade = ano_atual - ano_nascimento
-
+ 
     print(f"Você tem {idade} anos.")
 
-def calcular_compra():
-    total = 0
-    while True:
-        preco_item = float(input("digite"))
 
 def calcular_preco_compra():
-    
     total = 0
     while True:
-        try:
-            preco_str = input("Digite o preço do item (ou 'f' para finalizar): R$ ")
-            if preco_str.lower() == 'f':
+            preco_str = input("Digite o preço do item (ou 0 para finalizar): R$ ")
+            if preco_str == 4:
                 break
-            preco = float(preco_str)
-            if preco < 0:
-                print("O preço não pode ser negativo. Tente novamente.")
-                continue
-            total += preco
-        except ValueError:
+            total += preco_str
+            print("O preço não pode ser negativo. Tente novamente.")
             print("\nEntrada inválida. Por favor, digite um preço válido ou 'f' para finalizar.\n")
     print(f"\nO preço total da compra é: R$ {total:.2f}\n")
 
 
 def menu_principal():
+    while True:
+        print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
+        print("🟩    MENU DO PROGRAMA   I")
+        print("🟩           DO          I")
+        print("🟩         CAUAN         I")
+        print("🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩")
+
+        opcao = input("Escolha uma opcao (1, 2 ou 3):")
 
 
+        print("(1)-calcular_idade (2)- calcular_preco_da_compra (3)- sair ")
+       
 
-opcao = input("Escolha uma opção: ")
-
-print("")
-print("(1)-calcular_idade (2)- calcular_preço_da _compra (3)- sair ")
-print("")
-nível = int(input("Escolha um nível"))
-
-if (nível == 1): 
-    calcular_idade()
-elif (nível == 2): 
-    calcular_preco_compra()
-elif (nível == 3): 
-    print("Saindo do programa. Até mais!")
-    break
-else:
-print("\nOpção inválida. Por favor, escolha 1, 2 ou 3.\n")
-
-if __name__ == "__main__":
+        if opcao == '1':
+            calcular_idade()
+        elif opcao == '2':
+            calcular_preco_compra
+        elif opcao == '3':
+            print("sair do programa")
+            break
+        else:
+            print("\nOpção invalida. por favor escolha 1, 2 ou 3.\n")
+           
+if __name__=="__main__":
     menu_principal()
